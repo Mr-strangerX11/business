900b0e6aede83e5541822ef08231c5775b2998b5
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-01-02 03:57:49 +0545

    Build for GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wolf_alpha/',
+  base: '/business/',
   server: {
     port: 3000,
     proxy: {
